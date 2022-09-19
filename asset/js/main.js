ff24cf505e88cb50d2f96f7cd3c1e5b694aa4ee5
thong <thongva@regit-technology.com>
2022-09-19 14:48:38 +0700

    check2

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,12 +6,11 @@ var heightHeader = header.offsetHeight;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
 
-    if (prevScrollPos > currentScrollPos) { // Srcoll up    
+    if (prevScrollPos > currentScrollPos) { // Scroll up    
         if (currentScrollPos <= heightHeader) {
             header.classList.remove("scroll-up");
             header.classList.add("scroll-down");
-        }
-        else {
+        }else {
             header.classList.add("scroll-up");
             header.classList.remove("scroll-down");
         }
@@ -20,13 +19,12 @@ window.onscroll = function () {
         header.classList.add("scroll-down");
     }
     prevScrollPos = currentScrollPos;
-
 }
 
-/* Actice pagition when click */
-var pagitionNumbers = document.querySelectorAll('.pagination-number');
+/* Active pagination when click */
+var paginationNumbers = document.querySelectorAll('.pagination-number');
 
-pagitionNumbers.forEach(function (number) {
+paginationNumbers.forEach(function (number) {
     number.onclick = function () {
         document.querySelector('.pagination-number.active').classList.remove('active');
         this.classList.add('active');
@@ -43,8 +41,6 @@ var logoText = document.querySelector('.name-company');
 
     btnSearch.addEventListener("click", function(event){
         event.preventDefault();
-        console.log('tab search', tabSearch);
-        console.log('btnToggle', btnToggle);
         headerSearch.classList.toggle("display");
         tabSearch.classList.toggle("display");
         logoImg.classList.toggle("hide");
